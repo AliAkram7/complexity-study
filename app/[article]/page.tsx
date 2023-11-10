31c48af7e7e0833f966825412698c132f68f248a
Barka Akram <soulimanali2323@gmail.com>
2023-11-10 01:56:30 +0100

    fixing types

--- app/[article]/page.tsx
+++ app/[article]/page.tsx
@@ -4,31 +4,40 @@ import { PostgrestSingleResponse, createClient } from '@supabase/supabase-js'
 import classes from './article.module.css'
 import { Center } from '@mantine/core'
 import NotFound from './not-found'
+import { supabase } from '../../utiles/supabase'
 
 type Article = {
   article_title: string,
   article_description: string,
   address_link: string,
-  description_list: { label: string, description: string }[] ,
-  content_table: { label: string, link:string, order: number }[],
+  description_list: { label: string, description: string }[],
+  content_table: { label: string, link: string, order: number }[],
   article_content: string
 }
 
 
+
+export async function generateStaticParams() {
+
+
+  let { data } : any  = await supabase.from('article').select('address_link')
+
+  let links = data?.map((item : any) => {
+    return { article: item.address_link }
+  })
+
+  return links
+
+}
+
 export default async function Sort({ params }: { params: { article: string } }) {
 
-  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL || "", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "")
 
 
-
-
-
-  let { data } : PostgrestSingleResponse<Article> = await supabase.from('article').select('*')
+  let { data }: PostgrestSingleResponse<Article> = await supabase.from('article')
+    .select('*')
     .eq('address_link', params.article)
     .single()
-
-
-    
 
 
 
